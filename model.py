22747c98ce2195e3b4849271ae01407d17a7041b
peterhn <peterhn2@uci.edu>
2017-03-05 02:48:22 -0800

    updated model nvidia

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ from keras import backend as K
 from keras.applications.vgg16 import VGG16
 
 from scipy.misc import imread
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
@@ -17,6 +18,26 @@ import pandas as pd
 
 import cv2, numpy as np
 
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
 
 def LeNet():
     model = Sequential()
@@ -36,39 +57,20 @@ def LeNet():
     model.compile(loss='mse', optimizer='adam')
     return model
 
-def get_model():
-    h,w,c=32,64,3
-
+def NvidiaNet():
     model = Sequential()
-    model.add(Lambda(lambda x: x/127.5 - 1.,
-              input_shape=(h,w,c),
-              output_shape=(h,w,c)))
-    model.add(Convolution2D(3, 1, 1, subsample=(1, 1), border_mode='same',
-                            init = 'he_normal'))
-    model.add(BatchNormalization())
-    model.add(ELU())
-    model.add(Convolution2D(16, 5, 5, subsample=(4, 4), border_mode="same",
-                            init = 'he_normal'))
-    model.add(BatchNormalization())
-    model.add(ELU())
-    model.add(Convolution2D(32, 3, 3, subsample=(2, 2), border_mode="same",
-                            init = 'he_normal'))
-    model.add(BatchNormalization())
-    model.add(ELU())
-    model.add(Convolution2D(64, 3, 3, subsample=(2, 2), border_mode="same", 
-                            init = 'he_normal'))
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((70,25), (0,0))))
+    model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
     model.add(Flatten())
-    model.add(Dropout(.2))
-    model.add(BatchNormalization())
-    model.add(ELU())
-    model.add(Dense(512))
-    model.add(Dropout(.5))
-    model.add(BatchNormalization())
-    model.add(ELU())
+    model.add(Dense(120))
+    model.add(Dense(77))
     model.add(Dense(1))
-
-    model.compile(optimizer="adam", loss="mse")
-
+    model.compile(loss='mse', optimizer='adam')
     return model
 
 
@@ -121,8 +123,8 @@ for i in range(nb_classes):
 
 # model = AlexNet()
 # model = VGG_16()
-model = LeNet()
-# model = get_model()
+# model = LeNet()
+model = NvidiaNet()
 #X_train, X_val, y_train, y_val = train_test_split(car_images, steering_angles, test_size=0.33, random_state=0)
 X_train, y_train = shuffle(car_images, steering_angles) 
 
